9ae4e377327ad287a01cfd2cd8e87972abb721e0
Caroline Twigg <caroline@metismachine.com>
2018-01-02 23:40:15 -0500

    add command struct and commandList [#153513497]

---
+++ src/dispatch/dispatch.cpp
@@ -0,0 +1,26 @@
+#include "dispatch.h"
+#include <iostream> 
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <typeinfo>
+#include <typeindex>
+#include <cassert>
+#include "common.h"
+#include "auth/auth.h"
+#include "version.h"
+#include "logs/logs.h"
+#include "env/env.h"
+#include "templates/templates.h"
+#include "project/project.h"
+
+using namespace std;
+
+struct command setupCmd = {"setup", {}, false};
+struct command initCmd = {"init", {"--template"}, true};
+struct command authCmd = {"auth", {}, false};
+struct command templatesCmd = {"templates", {"--update", "--search"}, true};
+struct command logsCmd = {"logs", {"-n", "--tail"}, true};
+struct command commandList[5] = {setupCmd, initCmd, authCmd, templatesCmd, logsCmd};
+
