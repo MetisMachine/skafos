5ea8d2be93a07669a82417facd820de270f9d114
Wess Cope <wcope@me.com>
2018-01-16 11:16:20 -0500

    Fixes type-o in curl url

--- src/data/data.cpp
+++ src/data/data.cpp
@@ -25,8 +25,8 @@ void Data::fetch(string table) {
     console::info(Json(i).dump());
   }
 
-  console::info("\nCURL Request:");
-  string curl_cmd = "curl -D - -H \"X-API-Token: " + Env::instance()->get(METIS_API_TOKEN) + "\" -H \"Accept: application/json\" http://api.metismachine.io/v1/data/8e406744fb8f3c9d7d19fa03/" + table;
+  console::info("CURL Request:");
+  string curl_cmd = "curl -D - -H \"X-API-Token: " + Env::instance()->get(METIS_API_TOKEN) + "\" -H \"Accept: application/json\" http://api.metismachine.io/v1/data/" + PROJECT_TOKEN +"/" + table;
   console::info(curl_cmd);
   
 }
