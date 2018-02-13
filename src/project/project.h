fabcfa778936a11079066ddd73da5a187764848f
Caroline Twigg <caroline@metismachine.com>
2018-02-13 10:20:10 -0500

    add kill function [#154968412]

--- src/project/project.h
+++ src/project/project.h
@@ -7,6 +7,7 @@
 class Project {
 public:
   static void init(std::string name, std::string tpl);
+  static void kill(std::string kill_id, std::string task_type);
 
 private:
   static bool exists(std::string directory);
