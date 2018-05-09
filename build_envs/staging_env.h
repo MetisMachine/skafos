f3cf8961cfd3e4959211b4634cf10639c403dcc6
Caroline Twigg <caroline@metismachine.com>
2018-05-09 15:18:05 -0400

    add the vasi url for remote add to the env files

--- build_envs/staging_env.h
+++ build_envs/staging_env.h
@@ -8,5 +8,6 @@ const std::string ENVIRONMENT = "staging";
 const std::string CLIENT_ID   = "f1f6e59f3f6f8ffecde29d34ad18f673";
 const std::string API_URL     = "https://api.metis.wtf/v1";
 const std::string LOGGING_URL = "http://argus.metis.wtf/logs/";
+const std::string VASI_URL    = "http://vasi.metis.wtf/";
 
 #endif
