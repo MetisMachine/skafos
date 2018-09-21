aef2a2930d3fd689fae370220ca39050e7fd2d7e
Jeremy Tregunna <jeremy@tregunna.ca>
2018-09-21 07:06:51 -0600

    change logging host to api.metis.wtf for staging

--- build_envs/staging_env.h
+++ build_envs/staging_env.h
@@ -7,7 +7,7 @@
 const std::string ENVIRONMENT = "staging";
 const std::string CLIENT_ID   = "f1f6e59f3f6f8ffecde29d34ad18f673";
 const std::string API_URL     = "https://api.metis.wtf/v1";
-const std::string LOGGING_URL = "https://argus.metis.wtf/logs/";
+const std::string LOGGING_URL = "https://api.metis.wtf/logs/";
 const std::string VASI_URL    = "https://vasi.metis.wtf/";
 
 #endif
