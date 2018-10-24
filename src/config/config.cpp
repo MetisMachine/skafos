12b6af36876840f0bb72ff2490cb5cc7105142be
Caroline Twigg <carolinetwigg@ip-192-168-1-81.ec2.internal>
2018-10-24 11:02:17 -0400

    write yaml node to file

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -24,3 +24,8 @@ YAML::Node Config::load_json(Json json) {
     cout << "could not load json" << endl;
   }
 }
+
+void Config::yaml_to_file(YAML::Node node, std::string output_path) {
+  std::ofstream fout(output_path);
+  fout << node;
+}
