44b77623ebf01722236aababcc7f95f7cf5389de
Caroline Twigg <carolinetwigg@ip-192-168-1-81.ec2.internal>
2018-11-08 15:40:48 -0500

    add download models function to header

--- src/models/models.h
+++ src/models/models.h
@@ -4,6 +4,9 @@
 #include "common.h"
 #include "project_env/project_env.h"
 #include "request/request.h"
+#include "env/env.h"
+#include "file/file.h"
+#include <curl/curl.h>
 
 using namespace std;
 using namespace json11;
@@ -11,6 +14,10 @@ using namespace json11;
 class Models {
   public:
     static void list(std::string project_token, std::map<string, string> params);
+    static void download(std::string project_token, std::map<string,string> params, std::string output_path);
+
+  private:
+    static string tags_to_string(Json model_tags);
 };
 
 #endif
